1fe54fdf4410f72458e35daddca9a2bab5c655bc
azharuddinj <azharuddinj@cybage.com>
2021-05-18 19:08:19 +0530

    Adding assignment code

--- Assigningment1/src/app/app.module.ts
+++ Assigningment1/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DefaultModule} from './layouts/default/default.module';
+import { AppRoutingModule } from './app-routing.module';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    DefaultModule,
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
